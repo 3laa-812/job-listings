2c7dd3994f43374db15686e94effeb4ebed38d43
3laa <3laa.r.812@gmail.com>
2025-06-09 21:01:11 +0300

    update images path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 const Header = () => {
-  return <div className="bg-primary-dark-cyan">
-    <img src="../../public/images/bg-header-desktop.svg" alt="header" />
-  </div>;
+  return (
+    <div className="bg-primary-dark-cyan">
+      <img src="/src/assets/images/bg-header-desktop.svg" alt="header" />
+    </div>
+  );
 };
 
 export default Header;
